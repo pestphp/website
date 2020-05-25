27302a1cfc2078b465e4e19531a1dd74b81e16a5
Vaggelis Yfantis <me@octoper.me>
2020-05-25 17:53:43 +0300

    Made some visual changes to documentation

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -5,13 +5,13 @@
 @endsection
 
 @section('body')
-<section class="container max-w-8xl mx-auto px-6 md:px-8 py-4">
+<section class="container documentation max-w-8xl mx-auto px-6 md:px-8 py-4">
     <div class="flex flex-col lg:flex-row">
         <nav id="js-nav-menu" class="nav-menu hidden lg:block">
             @include('_nav.menu', ['items' => $page->navigation])
         </nav>
 
-        <div class="relative DocSearch-content w-full lg:w-3/5 break-words pb-16 lg:pl-4" v-pre>
+        <div class="relative DocSearch-content mt-8 w-full lg:w-3/5 break-words pb-16 lg:ml-6" v-pre>
             @yield('content')
             <div class="absolute top-0 right-0 h-8 w-100 pt-3 hidden md:block">
                 <a href="https://github.com/pestphp/website/edit/master/source{{ $page->getPath() }}.md">Edit this page →</a>
