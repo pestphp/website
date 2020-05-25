549eac7d0f83d5a389dfdcbea6bd75098097026e
Vaggelis Yfantis <me@octoper.me>
2020-05-25 17:56:45 +0300

    Added dns prefeching & move the main.css here

--- source/_layouts/headers.blade.php
+++ source/_layouts/headers.blade.php
@@ -1,3 +1,9 @@
+<link rel="dns-prefetch" href="https://fonts.googleapis.com/">
+<link rel="dns-prefetch" href="https://code.jquery.com/">
+@if($page->production)
+<link rel="dns-prefetch" href="https://www.googletagmanager.com/">
+@endif
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -18,15 +24,20 @@
     <meta name="generator" content="tighten_jigsaw_doc">
 @endif
 
+<link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+
 <title>{{ $page->siteName }}{{ $page->title ? ' | ' . $page->title : '' }}</title>
 
 <link rel="home" href="{{ $page->baseUrl }}">
+
 <script
   src="https://code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
   crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,700&display=swap" rel="stylesheet">
+
 <link rel="icon" href="/favicon.ico">
+
 <style type="text/css">
     .text-raleway {
         font-family: 'Raleway', sans-serif;
