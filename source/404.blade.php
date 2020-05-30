6a5e6478c0e3c805344d16cae3d84115c7e4d8b5
Vaggelis Yfantis <me@octoper.me>
2020-05-30 16:42:05 +0300

    Updated 404.blade.php

--- source/404.blade.php
+++ source/404.blade.php
@@ -4,12 +4,12 @@
 <div class="flex flex-col items-center mt-32 text-gray-700">
     <h1 class="text-6xl font-light leading-none mb-2">404</h1>
 
-    <h2 class="text-3xl">Page not found</h2>
+    <h2 class="text-2xl lg:text-3xl font-sans">Sorry! This page doesn't exist 😔</h2>
 
-    <hr class="block w-full max-w-lg mx-auto my-8 border">
+    <hr class="block max-w-lg mx-auto my-8 px-10 border">
 
     <p class="text-xl">
-        Need to update this page? See the <a title="404 Page Documentation" href="https://jigsaw.tighten.co/docs/custom-404-page/"> Jigsaw documentation</a>.
+        Let's go <a href="{{ $page->baseUrl }}">Home</a>
     </p>
 </div>
 @endsection
