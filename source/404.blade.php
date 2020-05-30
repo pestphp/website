27dbcaff5f096b757cd2354d9e38cae0a232c467
Vaggelis Yfantis <me@octoper.me>
2020-05-30 17:41:51 +0300

    Updated team.blade.ph

--- source/404.blade.php
+++ source/404.blade.php
@@ -4,7 +4,7 @@
 <div class="flex flex-col items-center mt-32 text-gray-700">
     <h1 class="text-6xl font-light leading-none mb-2">404</h1>
 
-    <h2 class="text-2xl lg:text-3xl font-sans">Sorry! This page doesn't exist 😔</h2>
+    <h2 class="text-2xl lg:text-3xl">Sorry! This page doesn't exist 😔</h2>
 
     <hr class="block max-w-lg mx-auto my-8 px-10 border">
 
--- source/docs/team.blade.php
+++ source/docs/team.blade.php
@@ -8,13 +8,25 @@ title: "Team"
 <h1>Meet the team</h1>
 
 <blockquote>
-	<p><strong>Hey!</strong> Want to be part of the team?
-	<br>We have some open team positions <a href="#open-positions">take a look.</a></p>
+	<p><strong>Hey!</strong> You like Pest and want to be part of the team?
+	<br>Reach out to us on <a href="https://discord.com/invite/bMAJv82">Discord</a> if you can help with any of the following:</p>
 </blockquote>
+
+<div id="open-positions">
+	<h3 class="text-2xl lg:text-3xl">Here are some open positions</h3>
+	<p class="text-gray-700 text-lg mb-6">We will be happy to join us!</p>
+
+	<ul class="list-inside text-lg">
+		<li>Discord Moderator</li>
+		<li>Documentation Moderator</li>
+		<li>Tester</li>
+		<li>Issue Triage</li>
+	</ul>
+</div>
 
 @foreach($page->team as $position => $team)
 <div class="mt-20">
-	<h3 class="text-2xl lg:text-3xl mb-1 ">{{ $position }}</h3>
+	<h3 class="text-2xl lg:text-3xl mb-1">{{ $position }}</h3>
 	<p class="text-gray-700 text-lg mb-16">{{ $team->description }}</p>
 
 	<div class="grid grid-cols-2 lg:grid-cols-3 gap-4 row-gap-16">
@@ -27,15 +39,5 @@ title: "Team"
 
 <hr>
 
-<div id="open-positions">
-	<h3 class="text-2xl lg:text-3xl mb-1">Here are some open positions</h3>
-	<p class="text-gray-700 text-lg mb-16">We will be happy to join us!</p>
 
-	<ul class="list-inside text-lg">
-		<li>Discord Moderator</li>
-		<li>Documentation Moderator</li>
-		<li>Tester</li>
-		<li>Issue Triage</li>
-	</ul>
-</div>
 @endsection
